a27400949d7674f14be8bd0dc6e90f3b50c04d76
Natalie Tapias <nataliemtapias@gmail.com>
2019-09-03 16:30:16 -0700

    Defined a child-class of Standard Error called InvalidStatusError and raise whenever a status other than :available or :unavailable is applied to a room.

--- lib/room.rb
+++ lib/room.rb
@@ -1,12 +1,23 @@
-# require_relative "date_range"
+require_relative "date_range"
 
 
 class Room
   attr_reader :id, :status
   
   def initialize(id, status=:available)
+    incorrect_status?(status)
     @id = id
     @status = status
+    
   end
+  
+  def incorrect_status?(given_status)
+    if given_status != :available && given_status != :unavailable
+      raise InvalidStatusError
+    end
+  end
+end
+
+class InvalidStatusError < StandardError
   
 end
--- test/room_test.rb
+++ test/room_test.rb
@@ -1,8 +1,10 @@
 require_relative "test_helper.rb"
 
 describe "Room_test#initialize" do
-  let(:r1){ Room.new(1,:unavailable)}
-  let(:r2){ Room.new(2)}
+  let(:r1){ Room.new(1,:unavailable) }
+  let(:r2){ Room.new(2) }
+  let(:r3){ Room.new("hi") }
+  let(:r4){ Room.new(1, :hello) }
   
   it "When given an id and status, returns an instance of Room" do
     expect(r1).must_be_instance_of Room
@@ -15,7 +17,13 @@ describe "Room_test#initialize" do
   it "Returns expected Room.status" do
     expect(r1.status).must_equal :unavailable
     expect(r2.status).must_equal :available
-    
   end
   
+  it "Raises some kind of error if non-integer is provided as the ID" do
+    expect(r3.id).must_be :==,"hi"
+  end
+  
+  it "Raises ArgumentError with any invalid status" do
+    expect{r4}.must_raise InvalidStatusError
+  end
 end
