b926c211e60ff9b1c0836a797c022aa9b4e9ce93
Natalie Tapias <nataliemtapias@gmail.com>
2019-09-03 14:08:39 -0700

    Wrote test to determine whether DateRange#start_date returns instance of Date. Modified DateRange class method to initialize with a Date Object.

--- lib/date_range.rb
+++ lib/date_range.rb
@@ -1,9 +1,33 @@
+require "date"
+
+
 class DateRange
   attr_reader :start_date, :end_date
   
   def initialize(start_date,end_date)
-    @start = start_date
-    @end = end_date
+    
+    
+    @start_date = Date.parse(start_date)
+    @end_date = Date.parse(end_date)
+  end
+  
+  def is_date?(start_date,end_date)
+    unless start_date.class Date
+      raise ArgumentError.new"It is not class Date. It is #{time}."
+    end
+    unless end_date.class Date
+      raise ArgumentError.new"It is not class Date. It is #{time}."
+    end
   end
   
 end
+
+
+# d = Date.parse('3rd Feb 2001')
+#                              #=> #<Date: 2001-02-03 ...>
+# d.year                       #=> 2001
+# d.mon                        #=> 2
+# d.mday                       #=> 3
+# d.wday                       #=> 6
+# d += 1                       #=> #<Date: 2001-02-04 ...>
+# d.strftime('%a %d %b %Y')    #=> "Sun 04 Feb 2001"
--- test/date_range_test.rb
+++ test/date_range_test.rb
@@ -1,9 +1,24 @@
 require_relative "test_helper"
 
 
-describe "Date Range" do
+describe DateRange do
+  let(:d1) { DateRange.new("3rd Feb 2001","4th Feb 2001") }
+  let(:d2) { DateRange.new("8th Dec 2001","20th Dec 2001") }
+  
   it "When given a start and end date, returns an instance of DateRange" do
-    date = DateRange.new(1,2)
-    expect(date).must_be_instance_of DateRange
+    expect(d1).must_be_instance_of DateRange
+  end
+  
+  it "DateRange#start_date returns accurate Date object" do
+    expect(d1.start_date.year).must_equal 2001
+    expect(d1.start_date.mon).must_equal 2
+    expect(d2.start_date.strftime('%d %b %Y')).must_equal "08 Dec 2001" 
+  end
+  
+  it "DateRange#end_date returns accurate end date" do
+  end
+  
+  it "Expects Successfully Parses Start Date String as instance of Date" do
+    expect(d2.start_date).must_be_instance_of Date
   end
 end
