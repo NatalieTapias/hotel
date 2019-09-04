4836b32a6c02e5b69b8526b3377e88acd7d8e0ce
Natalie Tapias <nataliemtapias@gmail.com>
2019-09-04 15:16:20 -0700

    Added a test for room ID too low (negative number). Test still passes.

--- test/room_test.rb
+++ test/room_test.rb
@@ -7,20 +7,22 @@ describe "Room" do
   let(:room_invalid_status){ Room.new(1, :hello) }
   let(:room_invalid_id_and_status){ Room.new("hi", :easy) }
   let(:room_id_too_high) { Room.new(30, :unavailable) }
+  let(:room_id_too_low) { Room.new(-30, :unavailable) }
   
   describe "initialize" do
     it "should return an instance of Room when valid id and valid status given" do
       expect(room_unavailable).must_be_instance_of Room
       expect(room_available).must_be_instance_of Room
+      expect{room_invalid_id}.must_raise ArgumentError
     end
   end 
   
   describe "id" do
-    it "should accurately return room ID" do
+    it "should accurately return :id" do
       expect(room_unavailable.id).must_equal 1
     end
     
-    it "Raises ArgumentError with any invalid ID provided" do
+    it "Raises ArgumentError with any invalid :id provided" do
       expect{room_id_too_high}.must_raise ArgumentError
       expect{room_invalid_id}.must_raise ArgumentError
     end
@@ -28,18 +30,20 @@ describe "Room" do
   end
   
   describe "status" do
-    it "Returns expected Room.status" do
+    it "should return accurate :status" do
       expect(room_unavailable.status).must_equal :unavailable
       expect(room_available.status).must_equal :available
     end
     
-    it "Raises InvalidStatusError with any invalid status" do
+    it "should raise ArgumentError when invalid :status is provided" do
       expect{room_invalid_status}.must_raise ArgumentError
+      expect{room_invalid_id_and_status}.must_raise ArgumentError
+      expect{room_id_too_low}.must_raise ArgumentError
     end
   end
   
   describe "cost" do
-    it "should accurately return room.cost" do
+    it "should accurately return :cost" do
       expect(room_unavailable.cost).must_equal 200
     end
   end
