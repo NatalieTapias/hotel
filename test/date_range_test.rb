982772998c48c19b0ce22e156fa4ac8796dfafc4
Natalie Tapias <nataliemtapias@gmail.com>
2019-09-04 15:22:16 -0700

    Modified instance variable names in date_range_test to be more descriptive.

--- test/date_range_test.rb
+++ test/date_range_test.rb
@@ -2,62 +2,62 @@ require_relative "test_helper"
 
 
 describe "DateRange#initialize" do
-  let(:d1) { DateRange.new("3rd Feb 2001","4th Feb 2001") }
-  let(:d2) { DateRange.new("8th Dec 2019","20th Dec 2019") }
-  let(:d3) { DateRange.new("Natalie","George") }
-  let(:d4) { DateRange.new("4th Feb 2001","2nd Feb 2001") }
-  let(:d5) { DateRange.new("4th Feb 2001","4th Feb 2001") }
+  let(:valid_dates) { DateRange.new("3rd Feb 2001","4th Feb 2001") }
+  let(:valid_dates_long_range) { DateRange.new("8th Dec 2019","20th Dec 2019") }
+  let(:invalid_dates) { DateRange.new("Natalie","George") }
+  let(:invalid_dates_start_before_end) { DateRange.new("4th Feb 2001","2nd Feb 2001") }
+  let(:invalid_dates_start_equals_end) { DateRange.new("4th Feb 2001","4th Feb 2001") }
   
   it "When given a start and end date, returns an instance of DateRange" do
-    expect(d1).must_be_instance_of DateRange
+    expect(valid_dates).must_be_instance_of DateRange
   end
   
   it "DateRange#start_date returns accurate Date object" do
-    expect(d1.start_date.year).must_equal 2001
-    expect(d1.start_date.mon).must_equal 2
-    expect(d2.start_date.strftime('%d %b %Y')).must_equal "08 Dec 2019" 
+    expect(valid_dates.start_date.year).must_equal 2001
+    expect(valid_dates.start_date.mon).must_equal 2
+    expect(valid_dates_long_range.start_date.strftime('%d %b %Y')).must_equal "08 Dec 2019" 
   end
   
   it "DateRange#end_date returns accurate end date" do
-    expect(d2.end_date.year).must_equal 2019
-    expect(d2.end_date.mon).must_equal 12
-    expect(d1.end_date.strftime('%d %b %Y')).must_equal "04 Feb 2001"
+    expect(valid_dates_long_range.end_date.year).must_equal 2019
+    expect(valid_dates_long_range.end_date.mon).must_equal 12
+    expect(valid_dates.end_date.strftime('%d %b %Y')).must_equal "04 Feb 2001"
   end
   
   it "Expects Successfully Parses Start Date String as instance of Date" do
-    expect(d2.start_date).must_be_instance_of Date
+    expect(valid_dates_long_range.start_date).must_be_instance_of Date
   end
   
   it "Raises ArgumentError when arguments are not appropriately provided" do
-    expect{d3}.must_raise ArgumentError 
+    expect{invalid_dates}.must_raise ArgumentError 
   end
   
   it "Raises a special error when invalid date range is provided (same date)" do
-    expect{d4}.must_raise InvalidDateRangeError
+    expect{invalid_dates_start_before_end}.must_raise InvalidDateRangeError
   end
   
   it "Raises a special error when invalid date range is provided (end_date is before start_date)" do
-    expect{d5}.must_raise InvalidDateRangeError
+    expect{invalid_dates_start_equals_end}.must_raise InvalidDateRangeError
   end
   
   it "Accurately calculates length_of_stay" do
-    expect(d1.length_of_stay).must_equal 1
-    expect(d2.length_of_stay).must_equal 12
+    expect(valid_dates.length_of_stay).must_equal 1
+    expect(valid_dates_long_range.length_of_stay).must_equal 12
   end 
   
   it "Should return true or false if date is within range" do
     # the below are true
     # first
-    expect(d2.contains_date?("8th Dec 2019")).must_equal true
+    expect(valid_dates_long_range.contains_date?("8th Dec 2019")).must_equal true
     # last
-    expect(d2.contains_date?("20th Dec 2019")).must_equal true
+    expect(valid_dates_long_range.contains_date?("20th Dec 2019")).must_equal true
     # middle
-    expect(d2.contains_date?("15th Dec 2019")).must_equal true
+    expect(valid_dates_long_range.contains_date?("15th Dec 2019")).must_equal true
     
     # the below are false
     # too far out 
-    expect(d2.contains_date?("30th Jan 2020")).must_equal false
+    expect(valid_dates_long_range.contains_date?("30th Jan 2020")).must_equal false
     # too far early
-    expect(d2.contains_date?("22nd May 2018")).must_equal false
+    expect(valid_dates_long_range.contains_date?("22nd May 2018")).must_equal false
   end
 end
